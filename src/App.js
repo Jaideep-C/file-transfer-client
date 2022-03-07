55443a10001b80f997bc0cf88872274d7ef18d0e
JAIDEEP_C <jaideepcvl@gmail.com>
2022-03-07 21:00:25 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./styles/app.css";
 function App() {
+  const [fileData, setFileData] = useState();
+  const onFileChange = (e) => setFileData(e.target.files[0]);
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData();
+    const server = "https://neat-otter-84.loca.lt";
+    data.append("fileData", fileData);
+    fetch(`${server}/file/upload`, {
+      method: "POST",
+      body: data,
+    })
+      .then((res) => console.log("Posted", res))
+      .catch((err) => console.log(err));
+    console.log(data);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="myApp">
+      <h1>Back up a file</h1>
+      <form onSubmit={onFormSubmit}>
+        <input type="file" onChange={onFileChange} />
+        <button type="Submit">Submit</button>
+      </form>
     </div>
   );
 }
